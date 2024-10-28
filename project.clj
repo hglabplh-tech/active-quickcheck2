c81c13f44c18791a70e0dd8c4ad7199d413fc872
Harald Glab-Plhak <harald.glab-plhak@t-online.de>
2024-10-28 17:43:54 +0000

    only for get the actual versions may be not needed

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "http://github.com/active-group/active-quickcheck2"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
                  ;; FIXME: only for active.monad, hopefully
-                 [de.active-group/active-clojure "0.42.0"]
+                 [de.active-group/active-clojure "0.43.0-SNAPSHOT"]
                  ;; FIXME: this is probably only expt, remove
                  [org.clojure/math.numeric-tower "0.1.0"]
-                 [de.active-group/active-data "0.1.0-SNAPSHOT"]]
+                 [de.active-group/active-data "0.2.1"]]
 
   :global-vars {*warn-on-reflection* true})
